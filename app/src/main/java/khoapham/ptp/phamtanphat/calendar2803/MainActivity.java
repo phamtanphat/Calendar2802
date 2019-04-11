06a68eb3de7764c59b9e677cc5ed36ffedac7f75
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-11 19:02:58 +0700

    a

---
+++ app/src/main/java/khoapham/ptp/phamtanphat/calendar2803/MainActivity.java
@@ -0,0 +1,28 @@
+package khoapham.ptp.phamtanphat.calendar2803;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Calendar calendar = Calendar.getInstance();
+        int ngay = calendar.get(Calendar.DATE);
+        int thang = calendar.get(Calendar.MONTH);
+        int gio = calendar.get(Calendar.HOUR_OF_DAY);
+
+        calendar.set(ngay,thang,calendar.get(Calendar.YEAR));
+
+        String time = new SimpleDateFormat("dd/MM/YYYY").format(Calendar.getInstance().getTime());
+
+        Log.d("BBB", time + "");
+    }
+}
