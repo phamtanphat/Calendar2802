458d8954c4258012127fd2be5ba78d85b51619f9
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-11 19:32:21 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/calendar2803/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/calendar2803/MainActivity.java
@@ -1,9 +1,13 @@
 package khoapham.ptp.phamtanphat.calendar2803;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -16,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     EditText edtSomin,edtSomax;
     Button btnDem;
     TextView txtNgay;
+    Calendar calendar1,calendar2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +30,38 @@ public class MainActivity extends AppCompatActivity {
         edtSomin = findViewById(R.id.edittextNgaymin);
         btnDem = findViewById(R.id.buttonDem);
         txtNgay = findViewById(R.id.textviewNgay);
+        calendar1 = Calendar.getInstance();
+        calendar2 = Calendar.getInstance();
 
+        edtSomin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        calendar1.set(year,month,dayOfMonth);
+                        Log.d("BBB",calendar1.getTimeInMillis() + "");
+                    }
+                },2019,4,11 );
+                datePickerDialog.show();
+
+            }
+        });
+        edtSomax.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        calendar2.set(year,month,dayOfMonth);
+
+                    }
+                },2019,4,11 );
+                datePickerDialog.getDatePicker().setMinDate(calendar1.getTimeInMillis());
+                datePickerDialog.show();
+
+            }
+        });
 
     }
 }
